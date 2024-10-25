d728236022ac13b962be50d008f6b02eb10fddac
taeiiin <87651203+taeiiin@users.noreply.github.com>
2024-10-25 15:41:10 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,22 @@
+N = int(input())
+num = list(map(int, input().split()))
+opt = list(map(int, input().split()))
+max_num = -1e9
+min_num = 1e9
+def dfs(dep, total, plus, minus, mul, div):
+    global max_num, min_num
+    if dep == N:
+        max_num = max(total, max_num)
+        min_num = min(total, min_num)
+        return
+    if plus:
+        dfs(dep + 1, total + num[dep], plus - 1, minus, mul, div)
+    if minus:
+        dfs(dep + 1, total - num[dep], plus, minus - 1, mul, div)
+    if mul:
+        dfs(dep + 1, total * num[dep], plus, minus, mul - 1, div)
+    if div:
+        dfs(dep + 1, int(total/num[dep]), plus, minus, mul, div - 1)
+
+dfs(1, num[0], opt[0], opt[1], opt[2], opt[3])
+print(max_num, min_num, sep='\n')
