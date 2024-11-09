83dfdedc9dfd3cde37cc1fb828eece8b7ff99229
taeiiin <87651203+taeiiin@users.noreply.github.com>
2024-11-09 18:01:11 +0900

    [level 3] Title: 입국심사, Time: 537.54 ms, Memory: 14.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,16 @@
+def solution(n, times):
+    answer = 0
+    left, right = 1, max(times)*n
+    while left <= right:
+        mid = (left + right)//2
+        p = 0
+        for t in times:
+            p += mid//t
+            if p >= n:
+                break
+        if p >= n:
+            answer = mid
+            right = mid - 1
+        elif p < n:
+            left = mid + 1
+    return answer
