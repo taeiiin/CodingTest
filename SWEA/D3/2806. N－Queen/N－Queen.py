641f306d436f9078921a17bb5098313fc49da2ec
taeiiin <87651203+taeiiin@users.noreply.github.com>
2024-11-16 21:34:52 +0900

    [D3] Title: N-Queen, Time: 289 ms, Memory: 50,944 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.py
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -1,23 +1,24 @@
-def dfs(row):
+def is_q(x):
+    for i in range(x):
+        if c[x] == c[i] or abs(c[x]-c[i]) == abs(x-i):
+            return False
+    return True
+
+def q(x):
     global result
-    if row == n:
+    if x == n:
         result += 1
         return
-    for i in range(n):
-        valid = True
-        for j in range(row):
-            if col[j] == i or abs(row - j) == abs(i - col[j]):
-                valid = False
-                break
-        if valid:
-            col[row] = i
-            dfs(row + 1)
-            col[row] = -1
+    else:
+        for i in range(n):
+            c[x] = i
+            if is_q(x):
+                q(x+1)
 
 T = int(input())
 for test_case in range(1, T + 1):
     n = int(input())
-    col = [-1]*n
     result = 0
-    dfs(0)
+    c = [0] * n
+    q(0)
     print("#%d %d" %(test_case, result))
